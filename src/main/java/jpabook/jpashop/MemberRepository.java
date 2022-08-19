61729f38dc1d9d5fc38806523fb52d0538122da0
ib1105 <ib1105@naver.com>
2022-08-20 01:50:26 +0900

    20220820

--- src/main/java/jpabook/jpashop/MemberRepository.java
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-@Repository
+//@Repository
 public class MemberRepository {
 
     @PersistenceContext
