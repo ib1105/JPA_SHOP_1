aa2f85b8272b7de436e1cd108206eeb09bf83558
ib1105 <ib1105@naver.com>
2022-08-20 08:13:32 +0900

    20220820_2

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -7,11 +7,13 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
-@Table(name="order_item")
-@Getter @Setter
+@Table(name = "order_item")
+@Getter
+@Setter
 public class OrderItem {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
 
@@ -23,6 +25,33 @@ public class OrderItem {
     @JoinColumn(name = "order_id")
     private Order order;
 
-    private int orderPrive;
+    private int orderPrice;
     private int count;
+
+    protected OrderItem() {
+
+    }
+
+
+    /*생성 메서드*/
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+
+        return orderItem;
+    }
+
+    /*비즈니스로직*/
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    /*주문상품가격 전체조회*/
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
